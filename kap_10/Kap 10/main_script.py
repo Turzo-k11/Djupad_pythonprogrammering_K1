1cc3c5ae26c9ce8d70cf7399c74ae5cbcac4971e
Turzo-k11 <148332731+Turzo-k11@users.noreply.github.com>
2024-09-10 20:37:14 +0200

    Add files via upload

---
+++ kap_10/Kap 10/main_script.py
@@ -0,0 +1,9 @@
+# main_script.py
+
+import my_module
+
+result_add = my_module.add(5, 3)
+result_subtract = my_module.subtract(10, 7)
+
+print("Addition result:", result_add)
+print("Subtraction result:", result_subtract)
